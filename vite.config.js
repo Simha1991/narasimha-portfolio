d952d090c6bf6aa99070f63e95c90080fbf1788b
Simha1991 <86404280+Simha1991@users.noreply.github.com>
2025-09-22 15:13:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/narasimha-portfolio/', // 👈 MUST match your GitHub repo name
+  base: '/narasimha-portfolio/', // 👈 important for GitHub Pages
 })
