42079a634c3f78320e01536149b624cb8b805cc9
Simha1991 <86404280+Simha1991@users.noreply.github.com>
2025-09-27 15:39:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/narasimha-portfolio/' : '/', // ✅ base only in prod
+  base: mode === 'production' ? '/narasimha-portfolio/' : '/',
 }))
