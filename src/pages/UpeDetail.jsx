993fc3bed83fae8ecede4cc10fe42cb45129b073
Simha1991 <86404280+Simha1991@users.noreply.github.com>
2025-09-27 14:37:58 +0530

    Update UpeDetail.jsx

--- src/pages/UpeDetail.jsx
+++ src/pages/UpeDetail.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function DepDetail() {
+export default function UpeDetail() {
   return (
     <section className="min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-gray-800">
