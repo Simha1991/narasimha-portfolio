b3359f4739c8f37687612916353a4189f5de8420
Simha1991 <86404280+Simha1991@users.noreply.github.com>
2025-09-27 14:24:21 +0530

    Create VaDetail.jsx

---
+++ src/pages/VaDetail.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function DepDetail() {
+  return (
+    <section className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">
+        VA – Detailed Work Coming Soon
+      </h1>
+    </section>
+  );
+}
