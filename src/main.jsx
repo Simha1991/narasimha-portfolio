910d8197e815cce4a67a94be77336bec81e84618
Simha1991 <86404280+Simha1991@users.noreply.github.com>
2025-09-27 14:50:49 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-// src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // ✅ import router
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/narasimha-portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
