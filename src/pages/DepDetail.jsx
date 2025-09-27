75348be0a28dc8b52dd1ffc4ea50dae45117f39c
Simha1991 <86404280+Simha1991@users.noreply.github.com>
2025-09-27 14:23:03 +0530

    Create DepDetail.jsx

---
+++ src/pages/DepDetail.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function DepDetail() {
+  return (
+    <section className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">
+        Digital Entry Point – Detailed Work Coming Soon
+      </h1>
+    </section>
+  );
+}
