bfc5434f7c289f40125a359f7be4ca28f1c3a2db
Simha1991 <86404280+Simha1991@users.noreply.github.com>
2025-09-27 14:23:58 +0530

    Create UpeDetail.jsx

---
+++ src/pages/UpeDetail.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function DepDetail() {
+  return (
+    <section className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">
+        UPE – Detailed Work Coming Soon
+      </h1>
+    </section>
+  );
+}
