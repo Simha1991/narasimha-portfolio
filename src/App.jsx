61778341d2dd6da744d888fa9162064198845069
Simha1991 <86404280+Simha1991@users.noreply.github.com>
2025-09-29 18:32:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ export default function App() {
                   <About />
                   <Skills />
                   <Featured />
-                  <Experienced />
+                  <Experience />
                   {/* other sections */}
                 </>
               }
