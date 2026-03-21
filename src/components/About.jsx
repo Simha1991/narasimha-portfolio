9f282ae338587145cb8736aa8e95014d32e0a55e
Simha1991 <86404280+Simha1991@users.noreply.github.com>
2026-03-21 11:34:30 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -35,7 +35,7 @@ export default function About() {
           {/* Image (25%) */}
           <div className="flex justify-center md:justify-end">
             <img
-              src="/headshot.jpg"  // 👉 make sure this exists in /public
+              src="/narasimha-portfolio/headshot.jpg"
               alt="Narasimha Bhat"
               className="w-40 h-40 md:w-48 md:h-48 object-cover rounded-xl shadow-md"
             />
